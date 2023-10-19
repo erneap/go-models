62a6521de17e4cf1f82cb584dbd2f82a5151bebd
Anton Erne <antonerne@yahoo.com>
2023-10-20 01:35:36 +0900

    Oct20-0135

--- teams/team.go
+++ teams/team.go
@@ -6,11 +6,13 @@ import (
 )
 
 type Team struct {
-	ID        primitive.ObjectID `json:"id" bson:"_id"`
-	Name      string             `json:"name" bson:"name"`
-	Workcodes []Workcode         `json:"workcodes" bson:"workcodes"`
-	Sites     []sites.Site       `json:"sites" bson:"sites"`
-	Companies []Company          `json:"companies,omitempty" bson:"companies,omitempty"`
+	ID             primitive.ObjectID `json:"id" bson:"_id"`
+	Name           string             `json:"name" bson:"name"`
+	Workcodes      []Workcode         `json:"workcodes" bson:"workcodes"`
+	Sites          []sites.Site       `json:"sites" bson:"sites"`
+	Companies      []Company          `json:"companies,omitempty" bson:"companies,omitempty"`
+	ContactTypes   []ContactType      `json:"contacttypes,omitempty" bson:"contacttypes,omitempty"`
+	SpecialtyTypes []SpecialtyType    `json:"specialies,omitempty" bson:"specialties,omitempty"`
 }
 
 type ByTeam []Team
