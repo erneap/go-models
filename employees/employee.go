b63e66a09459ff03131814115a288c4cb32e7836
Anton Erne <antonerne@yahoo.com>
2023-12-16 08:26:02 +0900

    Dec16-0825

--- employees/employee.go
+++ employees/employee.go
@@ -903,6 +903,7 @@ func (e *Employee) UpdateLeaveRequest(request, field, value string,
 					req.RequestedDays = append(req.RequestedDays, lv)
 				}
 			case "comment", "addcomment":
+				fmt.Println("add comment")
 				newComment := &LeaveRequestComment{
 					CommentDate: time.Now().UTC(),
 					Comment:     value,
