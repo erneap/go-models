0fc65f19013b722d91a99dc4917358d27f26ee1a
Anton Erne <antonerne@yahoo.com>
2023-12-15 09:31:03 +0900

    Dec15-0930

--- svcs/emailService.go
+++ svcs/emailService.go
@@ -39,6 +39,7 @@ func SendMail(to []string, subject, body string) error {
 		Password: config.Config("SMTP_PASS"),
 		From:     config.Config("SMTP_FROM"),
 	}
+	fmt.Println(smtpServer.Host)
 
 	err := smtpServer.Send(to, subject, body)
 	if err != nil {
