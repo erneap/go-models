5c9fbdc7912bab2e63b060fcf7019b40c54d38f8
Anton Erne <antonerne@yahoo.com>
2024-01-16 07:39:43 +0900

    Jan16-0739

--- employees/employee.go
+++ employees/employee.go
@@ -202,9 +202,7 @@ func (e *Employee) GetWorkday(date, lastWork time.Time) *Workday {
 	if work > 0.0 {
 		return wkday
 	}
-	if (date.Equal(lastWork) || date.Before(lastWork)) && work <= 0.0 {
-		return nil
-	}
+	wkday = nil
 
 	for _, lv := range e.Leaves {
 		if lv.LeaveDate.Year() == date.Year() &&
